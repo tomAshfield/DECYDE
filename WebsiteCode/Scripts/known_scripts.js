01978ab0360f31f06366c9d4a5b976932fe2f1e2
Tommy Ashfield <tomash@udel.edu>
2023-01-02 23:11:27 -0500

    Finished the logic for the right side

--- WebsiteCode/Scripts/known_scripts.js
+++ WebsiteCode/Scripts/known_scripts.js
@@ -1,35 +1,44 @@
 //List for the user to put their ideas in
 let ideas = ['Go to park', 'Make Dinner', 'Have fun', 'Cuddle bf'];
 let finalIdeas = [];
-let finalIdea = '';
+let finalIdea = [];
+let fIdea;
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //Takes in nothing; adds whatever they inputted into the text box
 function addIdeas(){
     let temp = ideas.map(idea => `<li>${idea}</li>`).join('\n');
-    document.querySelector('ul').innerHTML = temp;
+    document.querySelector('.ideaList').innerHTML = temp;
 }
 
 //Call function in order to display it on the website
 addIdeas();
 
 //Setting variables to event actions
-let btnAdd = document.querySelector('button');
 let input = document.querySelector('input');
 
 //Wating for the submit button to be clicked to then add the idea to the array list and website list
-btnAdd.addEventListener('click', () =>{
+function addIdea(){
     ideas.push(input.value);
     input.value = '';
     addIdeas();
-});
+}
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-let finalDesc = document.querySelector('FinalDescBtn');
+function FinalIdeas(){
+  finalIdea.push(fIdea)
+  let temp = finalIdea.map(idea => `<li>${idea}</li>`).join('\n');
+  document.querySelector('.FDList').innerHTML = temp;
+}
 
-finalDesc.addEventListener('click', () =>{
-    shuffle(ideas);
-});
+FinalIdeas();
+
+
+function nextStep(){
+  shuffle(ideas);
+  userPrompt();
+  FinalIdeas();
+}
 
 //Shuffle the ideas when the user is ready to move on
 function shuffle(array) {
@@ -49,19 +58,18 @@ function shuffle(array) {
     finalIdeas = array;
   }
 
+function userPrompt() {
+  let index = prompt("Enter a number between 1-" + finalIdeas.length, "");
+  fIdea = finalIdeas[parseInt(index - 1)];
+}
+
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //Hide/Show the final decision DIV
-function hideShow() {
-    var x = document.getElementById("FDList");
+/*function hideShow() {
+    var x = document.getElementById(".FDList");
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
       x.style.display = "none";
     }
-  }
-
-  function userPrompt() {
-    let index = prompt("Enter a number between 1-" + finalIdeas.length, "");
-    finalIdea = finalIdeas[parseInt(index - 1)];
-    document.querySelector('FDlist').innerHTML = finalIdea;
-  }
+  }*/
