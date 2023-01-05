0aec2b3fb9ce9678475b6bf4b3e9f9bb2d060b1a
Tommy Ashfield <tomash@udel.edu>
2023-01-04 20:36:53 -0500

    ADDED LOCATION YAAS

--- WebsiteCode/Scripts/suggestion_scripts.js
+++ WebsiteCode/Scripts/suggestion_scripts.js
@@ -1,8 +1,21 @@
 //Finds the users location when first loading the page
 let fLocation;
-const sucessfulLookup = (position) => {
-    const { latitude, longitude } = position.coords;
-    fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=bbf45608290b47a5bdae434efcc52c87`)
-    .then(response => response.json())
-    .then(fLocation = response);
-};
+window.onload = function() {
+    if (window.navigator.geolocation) {
+        const successfulLookup = (position) => {
+            const { latitude, longitude } = position.coords;
+            fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=bbf45608290b47a5bdae434efcc52c87`)
+            .then(response => response.json())
+            .then(data => {
+                document.getElementById('location').innerHTML =  data.results[0].formatted;
+            });
+        }
+    window.navigator.geolocation
+    .getCurrentPosition(successfulLookup, console.log);
+    
+    
+    }
+}
+
+//var text = document.createTextNode(fLocation);
+
